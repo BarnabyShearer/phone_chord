809a2b20739a0391f59bd12b6583bb60d5ac9f7c
Barnaby <b@zi.is>
2018-02-05 16:23:15 +0000

    working

--- phone_chord.ino
+++ phone_chord.ino
@@ -11,8 +11,8 @@
 
 #include <bluefruit.h>
 
-#define MODEL "Phone Chord v0.0.1"
-#define GRACE (20) //ms
+#define MODEL "PhoneChord 0.1"
+#define GRACE (70) //ms (well really 1s/1024)
 #define CONTRY (32) //UK
 
 typedef volatile uint32_t REG32;
@@ -30,7 +30,8 @@ typedef volatile uint32_t REG32;
 BLEDis bledis;
 BLEHidAdafruit blehid;
 
-uint8_t pins[]    = { A0, A1, A2, A3, A4, A5, 16, 15, 7, 11};
+TimerHandle_t grace_timer;
+uint8_t pins[]    = { A0, A1, A2, A3, 16, 15, 7, 11};
 uint8_t pincount = sizeof(pins)/sizeof(pins[0]);
 uint8_t modifier = 0;
 uint8_t keycode[6] = { 0 };
@@ -106,48 +107,7 @@ uint8_t hidcode[] = {
 
 void setup()
 {
-  for (uint8_t i=0; i<pincount; i++)
-  {
-    pinMode(pins[i], INPUT_PULLUP);
-    attachInterrupt(pins[i], input, CHANGE);
-  }
 
-  //For readVBAT
-  analogReference(AR_INTERNAL_3_0);
-  analogReadResolution(12);
-  
-  Serial.begin(115200);
-
-  Serial.println(MODEL);
-  Serial.println("");
-
-  // MAC Address
-  uint32_t addr_high = ((MAC_ADDRESS_HIGH) & 0x0000ffff) | 0x0000c000;
-  uint32_t addr_low  = MAC_ADDRESS_LOW;
-  Serial.print("MAC Address : ");
-  Serial.print((addr_high >> 8) & 0xFF, HEX); Serial.print(":");
-  Serial.print((addr_high) & 0xFF, HEX); Serial.print(":");
-  Serial.print((addr_low >> 24) & 0xFF, HEX); Serial.print(":");
-  Serial.print((addr_low >> 16) & 0xFF, HEX); Serial.print(":");
-  Serial.print((addr_low >> 8) & 0xFF, HEX); Serial.print(":");
-  Serial.print((addr_low) & 0xFF, HEX); Serial.println("");
-
-  // Unique Device ID
-  Serial.print("Device ID  : ");
-  Serial.print(DEVICE_ID_HIGH, HEX);
-  Serial.println(DEVICE_ID_LOW, HEX);
-
-  // MCU Variant;
-  Serial.printf("MCU Variant: nRF%X 0x%08X\n",NRF_FICR->INFO.PART, NRF_FICR->INFO.VARIANT);
-  Serial.printf("Memory     : Flash = %d KB, RAM = %d KB\n", NRF_FICR->INFO.FLASH, NRF_FICR->INFO.RAM);
-
-  // vBAT
-  Serial.printf("vBAT        : %.0fmv\n", (float)analogRead(VBAT_PIN) * VBAT_MV_PER_LSB * VBAT_DIVIDER_COMP);
-    
-  dbgPrintVersion();
-  
-  dbgMemInfo();
-  
   Bluefruit.begin();
   Bluefruit.setTxPower(4);
   Bluefruit.setName(MODEL);
@@ -171,13 +131,50 @@ void setup()
   Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
   Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds
 
-  //timer
-  NRF_TIMER1->TASKS_STOP = 1;
-  NRF_TIMER1->MODE = TIMER_MODE_MODE_Timer << TIMER_MODE_MODE_Pos;
-  NRF_TIMER1->BITMODE = TIMER_BITMODE_BITMODE_16Bit << TIMER_BITMODE_BITMODE_Pos;
-  NRF_TIMER1->PRESCALER = 9; //32us
-  NRF_TIMER2->CC[0] = 31 * GRACE;
-  attachInterrupt(TIMER1_IRQn, timer, CHANGE);
+  //INIT
+  for (uint8_t i=0; i<pincount; i++)
+  {
+    pinMode(pins[i], INPUT_PULLUP);    
+    attachInterrupt(pins[i], input, CHANGE);
+  }
+
+  grace_timer = xTimerCreate("Timer", GRACE, pdFALSE, 0, vTimerCallback);
+
+  //For readVBAT
+  analogReference(AR_INTERNAL_3_0);
+  analogReadResolution(12);
+  
+  Serial.begin(115200);
+
+  Serial.println(MODEL);
+  Serial.println("");
+
+  // MAC Address
+  uint32_t addr_high = ((MAC_ADDRESS_HIGH) & 0x0000ffff) | 0x0000c000;
+  uint32_t addr_low  = MAC_ADDRESS_LOW;
+  Serial.print("MAC Address     : ");
+  Serial.print((addr_high >> 8) & 0xFF, HEX); Serial.print(":");
+  Serial.print((addr_high) & 0xFF, HEX); Serial.print(":");
+  Serial.print((addr_low >> 24) & 0xFF, HEX); Serial.print(":");
+  Serial.print((addr_low >> 16) & 0xFF, HEX); Serial.print(":");
+  Serial.print((addr_low >> 8) & 0xFF, HEX); Serial.print(":");
+  Serial.print((addr_low) & 0xFF, HEX); Serial.println("");
+
+  // Unique Device ID
+  Serial.print("Device ID       : ");
+  Serial.print(DEVICE_ID_HIGH, HEX);
+  Serial.println(DEVICE_ID_LOW, HEX);
+
+  // MCU Variant;
+  Serial.printf("MCU Variant     : nRF%X 0x%08X\n",NRF_FICR->INFO.PART, NRF_FICR->INFO.VARIANT);
+  Serial.printf("Memory          : Flash = %d KB, RAM = %d KB\n", NRF_FICR->INFO.FLASH, NRF_FICR->INFO.RAM);
+
+  // vBAT
+  Serial.printf("vBAT            : %.0fmv\n", (float)analogRead(VBAT_PIN) * VBAT_MV_PER_LSB * VBAT_DIVIDER_COMP);
+    
+  dbgPrintVersion();
+  
+  dbgMemInfo();
 }
 
 void loop()
@@ -187,19 +184,18 @@ void loop()
 
 void input()
 {
-  NRF_TIMER1->TASKS_CLEAR = 1;
-  NRF_TIMER1->TASKS_START = 1;
+  xTimerResetFromISR(grace_timer, pdFALSE);
 }
 
-void timer()
+void vTimerCallback(TimerHandle_t xTimer)
 {
   uint8_t pressed = 0;
-  NRF_TIMER1->TASKS_STOP = 1;
   for (uint8_t i=0; i<pincount; i++)
   {
-    pressed += digitalRead(pins[i]) << i;
+    pressed += !digitalRead(pins[i]) << i;
   }
   modifier ^= modifiers[pressed];
   keycode[0] = hidcode[pressed];
   blehid.keyboardReport(modifier, keycode);
+  Serial.println(keycode[0]);
 }
--- practice.py
+++ practice.py
@@ -8,6 +8,8 @@ from brs001 import MOD, KEYS
 MOD = {v:k for k, v in MOD.items()}
 KEYS = {v:k for k, v in KEYS.items()}
 
+import keyboard
+
 INPUT = {
     30: 1,
     31: 2,
@@ -19,26 +21,76 @@ INPUT = {
     39: 5
 }
 
-GRACE = 0.100
+GRACE = 0.070
 
 dev = evdev.InputDevice('/dev/input/by-path/platform-i8042-serio-0-event-kbd')
 dev.grab()
 dev.repeat = evdev.device.KbdInfo(repeat=0, delay=99999) #often ignored
 
+ui = evdev.UInput()
 mods = set()
+held = set()
+
+def hid2key(key):
+    return evdev.ecodes.ecodes[
+        key.replace(
+            "HID_KEY_",
+            "KEY_"
+        ).replace(
+            "KEYBOARD_MODIFIER_",
+            "KEY_"
+        ).replace(
+            "KEY_PERIOD",
+            "KEY_DOT"
+        ).replace(
+            "KEY_RETURN",
+            "KEY_ENTER"
+        ).replace(
+            "GUI",
+            "META"
+        ).replace(
+            "ARROW_",
+            ""
+        ).replace(
+            "PAGE_",
+            "PAGE"
+        ).replace(
+            "ESCAPE",
+            "ESC"
+        ).replace(
+            "BRACKET_LEFT",
+            "LEFTBRACE"
+        ).replace(
+            "BRACKET_RIGHT",
+            "RIGHTBRACE"
+        )
+    ]
 
 def done():
+    global held
     pressed = set()
     for key in dev.active_keys():
-        pressed.add(INPUT[key])
+        if key in INPUT:
+            pressed.add(INPUT[key])
     pressed = frozenset(pressed)
     if pressed in MOD:
         if MOD[pressed] in mods:
             mods.remove(MOD[pressed])
+            ui.write(evdev.ecodes.EV_KEY, hid2key(MOD[pressed]), 0)
         else:
             mods.add(MOD[pressed])
+            ui.write(evdev.ecodes.EV_KEY, hid2key(MOD[pressed]), 1)
         return
-    print(KEYS.get(pressed, None), mods)
+    if pressed in KEYS:
+        if KEYS[pressed] not in held:
+            held.add(KEYS[pressed])
+            ui.write(evdev.ecodes.EV_KEY, hid2key(KEYS[pressed]), 1)
+    else:
+        for key in held:
+            ui.write(evdev.ecodes.EV_KEY, hid2key(key), 0)
+        held = set()
+    ui.syn()
+    #print(KEYS.get(pressed, None), mods)
 
 timer = None
 for event in dev.read_loop():
